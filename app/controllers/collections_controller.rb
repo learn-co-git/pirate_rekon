e43ed0fd898d8a77bbb214fb086b9f6a457da161
learn-co-git <argus.two.2@protonmail.com>
2020-06-06 14:47:50 -0700

    edit method, user select album

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -8,7 +8,7 @@ class CollectionsController < ApplicationController
   get '/collections/:user_id' do #display all the users collections
     #binding.pry
      @collection = Collection.all.select {|record| record.user_id == params[:user_id].to_i}
-     @images = Image.all
+     @user = User.find_by_id(session[:user_id])
      erb :show
   end
 
@@ -16,15 +16,21 @@ class CollectionsController < ApplicationController
     erb :new
   end
 
-  get 'images/new' do #add new image to existing collection
-    erb :images_new
-  end
+  # post '/images/new' do #add image to collection
+  #    @album = Collection.all.select {|record| record.user_id == session[:user_id]}[(params[:album].to_i - 1)]
+  #    redirect "/collections/#{@album.id}/edit"
+  # end
 
   post '/collections' do #create new collection
     @user_collection = Collection.new(:name => params[:collection], :creation_date => Time.now, :user_id => session[:user_id])
-    @user_collection.id = session[:user_id]
     @user_collection.save
     redirect to "/collections/#{@user_collection.user_id}"
+  end
+
+  get '/collections/:user_id/edit' do
+    binding.pry
+       @album = Collection.all.select {|record| record.user_id == session[:user_id]}
+    erb :edit
   end
 
 
